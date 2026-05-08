abb8808bfac25682c7dc39d00a31ccb577de7a31
Вова Савченко <vova-savchenko-06@mail.ru>
2026-05-08 20:32:20 +0300

    feat: the finished concepts page

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 import Header from "./Header.js"
 import Hero from "./Hero.js"
 import TrustSlider from "./TrustSlider.js"
+import Concepts from "./Concepts.js"
+import ExpandebleContent from "./ExpandebleContent.js"
 
 new Header()
 new Hero()
 new TrustSlider()
+new Concepts()
+new ExpandebleContent()
+
+
